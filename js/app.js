0ec84c6530aef011e58abd4d1bc44cbc9c09dbec
fedemrf92 <fedemrf92@gmail.com>
2021-04-22 22:09:25 -0300

    Agregue Vue

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    cafeterias: [
+      { nombre: "Sauco", estrellas: 4, comida: 4, precio: 4, lugar: 4 },
+      { nombre: "Cafe Dore", estrellas: 2, comida: 2, precio: 4, lugar: 4 },
+      { nombre: "Culto Cafe", estrellas: 5, comida: 2, precio: 4, lugar: 4 },
+    ],
+  },
+});
